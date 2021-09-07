a41846b05fb2af2e61794bd8fd5a3698b2448968
lodisperkins <lodisperkins44@gmail.com>
2021-09-07 16:03:45 -0500

    Updated exercise to include new concepts

--- BattleArena/Game.cs
+++ BattleArena/Game.cs
@@ -20,13 +20,15 @@ namespace BattleArena
         bool gameOver;
         int currentScene;
         Character player;
+        Character[] enemies;
+        private int currentEnemyIndex = 0;
+        private Character currentEnemy;
 
         /// <summary>
         /// Function that starts the main game loop
         /// </summary>
         public void Run()
         {
-            
         }
 
         /// <summary>
@@ -34,7 +36,6 @@ namespace BattleArena
         /// </summary>
         public void Start()
         {
-           
         }
 
         /// <summary>
@@ -42,7 +43,6 @@ namespace BattleArena
         /// </summary>
         public void Update()
         {
-            
         }
 
         /// <summary>
@@ -50,7 +50,6 @@ namespace BattleArena
         /// </summary>
         public void End()
         {
-            
         }
 
         /// <summary>
@@ -62,47 +61,73 @@ namespace BattleArena
         /// <returns></returns>
         int GetInput(string description, string option1, string option2)
         {
-            
+            string input = "";
+            int inputReceived = 0;
+
+            while (inputReceived != 1 && inputReceived != 2)
+            {//Print options
+                Console.WriteLine(description);
+                Console.WriteLine("1. " + option1);
+                Console.WriteLine("2. " + option2);
+                Console.Write("> ");
+
+                //Get input from player
+                input = Console.ReadLine();
+
+                //If player selected the first option...
+                if (input == "1" || input == option1)
+                {
+                    //Set input received to be the first option
+                    inputReceived = 1;
+                }
+                //Otherwise if the player selected the second option...
+                else if (input == "2" || input == option2)
+                {
+                    //Set input received to be the second option
+                    inputReceived = 2;
+                }
+                //If neither are true...
+                else
+                {
+                    //...display error message
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadKey();
+                }
+
+                Console.Clear();
+            }
+            return inputReceived;
         }
-        
+
+        /// <summary>
+        /// Calls the appropriate function(s) based on the current scene index
+        /// </summary>
+        void DisplayCurrentScene()
+        {
+        }
+
         /// <summary>
         /// Displays the menu that allows the player to start or quit the game
         /// </summary>
         void DisplayMainMenu()
         {
-            
         }
 
         /// <summary>
-        /// All code for the character selection screen should go here
+        /// Displays text asking for the players name. Doesn't transition to the next section
+        /// until the player decides to keep the name.
         /// </summary>
-        public void StartSceneCharacterSelection()
+        void GetPlayerName()
         {
-            
+
         }
 
         /// <summary>
-        /// All code for room 1 goes here
+        /// Gets the players choice of character. Updates player stats based on
+        /// the character chosen.
         /// </summary>
-        public void StartSceneRoom1()
+        public void CharacterSelection()
         {
-            
-        }
-
-        /// <summary>
-        /// All code for room 2 goes here
-        /// </summary>
-        public void StartSceneRoom2()
-        {
-            
-        }
-
-        /// <summary>
-        /// All code for room 3 goes here
-        /// </summary>
-        public void StartSceneRoom3()
-        {
-           
         }
 
         /// <summary>
@@ -111,7 +136,6 @@ namespace BattleArena
         /// <param name="character">The character that will have its stats shown</param>
         void DisplayStats(Character character)
         {
-            
         }
 
         /// <summary>
@@ -122,7 +146,6 @@ namespace BattleArena
         /// <returns>The amount of damage done to the defender</returns>
         float CalculateDamage(float attackPower, float defensePower)
         {
-            
         }
 
         /// <summary>
@@ -131,19 +154,24 @@ namespace BattleArena
         /// <param name="attacker">The character that initiated the attack</param>
         /// <param name="defender">The character that is being attacked</param>
         /// <returns>The amount of damage done to the defender</returns>
-        public float Fight(ref Character attacker, ref Character defender)
+        public float Attack(ref Character attacker, ref Character defender)
         {
-            
         }
 
         /// <summary>
-        /// Starts a battle between the player and the enemy.
-        /// Characters fight until one has had its health reduced to zero.
+        /// Simulates one turn in the current monster fight
         /// </summary>
-        /// <param name="enemy">The enemy the player is going to fight</param>
-        public void StartBattle(Character enemy)
+        public void Battle()
         {
-            
         }
+
+        /// <summary>
+        /// Checks to see if either the player or the enemy has won the current battle.
+        /// Updates the game based on who won the battle..
+        /// </summary>
+        void CheckBattleResults()
+        {
+        }
+
     }
 }
